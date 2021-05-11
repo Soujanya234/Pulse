d4a11d647e4b1b21d158b2ff3ae375d9500f69ce
Soujanya <mandadapusoujanya24@gmail.com>
2021-05-10 23:40:27 -0700

    Created a New Pulse Project

---
+++ Pulse/Pulse/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pulse
+//
+//  Created by Soujanya on 10/05/21.
+// Changes happened
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
